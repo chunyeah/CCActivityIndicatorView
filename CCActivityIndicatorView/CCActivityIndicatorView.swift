8243be7437902a1673fa9ca87b83c2a491e7829f
Chun Ye <yechunjun@gmail.com>
2014-12-11 17:38:41 +0800

    Update aniamtion from timer to Core Animation

--- CCActivityIndicatorView/CCActivityIndicatorView.swift
+++ CCActivityIndicatorView/CCActivityIndicatorView.swift
@@ -2,9 +2,7 @@
 //  CCLoadingView.swift
 //  CCLoading
 //
-//  Created by Chun Ye (chunforios@gmail.com)on 12/10/14.
-//  Github: https://github.com/yechunjun/CCActivityIndicatorView
-//  Blog: http://chun.tips
+//  Created by Chun Ye on 12/10/14.
 //  Copyright (c) 2014 Chun Tips. All rights reserved.
 //
 
@@ -12,15 +10,45 @@ import UIKit
 import Foundation
 
 struct Config {
-    static let CC_ACTIVITY_INDICATOR_VIEW_WIDTH = 40
-    static let CC_ARC_DRAW_PADDING = 3.0
-    static let CC_ARC_DRAW_DEGREE = 39.0
-    static let CC_ARC_DRAW_WIDTH = 6.0
-    static let CC_ARC_DRAW_RADIUS = 10.0
+    
+    static let CC_ACTIVITY_INDICATOR_VIEW_NUMBEROFFRAMES: Int = 8
+    static let CC_ACTIVITY_INDICATOR_VIEW_WIDTH: CGFloat = 40
+    static let CC_ARC_DRAW_PADDING: CGFloat = 3.0
+    static let CC_ARC_DRAW_DEGREE: CGFloat = 39.0
+    static let CC_ARC_DRAW_WIDTH: CGFloat = 6.0
+    static let CC_ARC_DRAW_RADIUS: CGFloat = 10.0
     static let CC_ARC_DRAW_COLORS = [UIColor(red: 242/255.0, green: 242/255.0, blue: 242/255.0, alpha: 1.0).CGColor, UIColor(red: 230/255.0, green: 230/255.0, blue: 230/255.0, alpha: 1.0).CGColor, UIColor(red: 179/255.0, green: 179/255.0, blue: 179/255.0, alpha: 1.0).CGColor, UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 1.0).CGColor, UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 1.0).CGColor, UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 1.0).CGColor, UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 1.0).CGColor, UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 1.0).CGColor]
+    
+    static func CCActivityIndicatorViewFrameImage(frame: Int, _ scale: CGFloat) -> UIImage {
+        
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(CC_ACTIVITY_INDICATOR_VIEW_WIDTH, CC_ACTIVITY_INDICATOR_VIEW_WIDTH), false, scale)
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        var startDegree = Config.CC_ARC_DRAW_PADDING
+        for index in 1...8 {
+            let arcPath = UIBezierPath()
+            let center  = CGPointMake(Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH / 2, Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH / 2)
+            let startAngle = CGFloat(DegreesToRadians(Double(startDegree)))
+            let endAngle = CGFloat(DegreesToRadians(Double(startDegree + Config.CC_ARC_DRAW_DEGREE)))
+            arcPath.addArcWithCenter(center, radius: Config.CC_ARC_DRAW_RADIUS, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+            CGContextAddPath(context, arcPath.CGPath)
+            startDegree += Config.CC_ARC_DRAW_DEGREE + (Config.CC_ARC_DRAW_PADDING * 2)
+            CGContextSetLineWidth(context, Config.CC_ARC_DRAW_WIDTH)
+            let colorIndex = abs(index - frame)
+            let strokeColor = Config.CC_ARC_DRAW_COLORS[colorIndex]
+            CGContextSetStrokeColorWithColor(context, strokeColor)
+            CGContextStrokePath(context)
+        }
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
 }
 
-// MARK: Math Helpers
+// MARK: Helpers
 
 func DegreesToRadians (value: Double) -> Double {
     return value * M_PI / 180.0
@@ -30,61 +58,70 @@ func DegreesToRadians (value: Double) -> Double {
 
 @IBDesignable class CCActivityIndicatorView: UIView {
     
-    /// default is true. calls -setHidden when animating gets set to NO
-    @IBInspectable var hidesWhenStopped : Bool = true
-    
-    func startAnimating () {
-        
-        if self.hidden {
-            self.hidden = false
-        }
-        
-        if let timer = self.animatedTimer {
-            timer.fire()
-        } else {
-            self.animatedTimer = NSTimer(timeInterval: 0.1, target: self, selector: "animate", userInfo: nil, repeats: true)
-            NSRunLoop.currentRunLoop().addTimer(self.animatedTimer!, forMode: NSRunLoopCommonModes)
-        }
-    }
-    
-    func stopAnimating () {
-        if let timer = self.animatedTimer {
-            timer.invalidate()
-            self.animatedTimer = nil
+    @IBInspectable var hidesWhenStopped : Bool = true {
+        didSet {
             if self.hidesWhenStopped {
-                if !self.hidden {
-                    self.hidden = true
-                }
+                self.hidden = !self.animating
+            } else {
+                self.hidden = false
             }
         }
     }
     
-    func isAnimating () -> Bool {
-        if let timer = self.animatedTimer {
-            return timer.valid
-        } else {
-            return false
+    func startAnimating () {
+        
+        func animate () {
+            self.animating = true
+            self.hidden = false
+            let animationDuration: CFTimeInterval = 0.8
+            var animationImages = [CGImageRef]()
+            
+            for frame in 1...8 {
+                animationImages.append(Config.CCActivityIndicatorViewFrameImage(frame, UIScreen.mainScreen().nativeScale).CGImage)
+            }
+            
+            let animation = CAKeyframeAnimation(keyPath: "contents")
+            animation.calculationMode = kCAAnimationDiscrete
+            animation.duration = animationDuration
+            animation.repeatCount = HUGE
+            animation.values = animationImages
+            animation.removedOnCompletion = false
+            animation.fillMode = kCAFillModeBoth
+            self.layer.addAnimation(animation, forKey: "contents")
         }
+        
+        if !self.animating {
+            animate()
+        }
+    }
+    
+    func stopAnimating () {
+        self.animating = false
+        
+        self.layer.removeAnimationForKey("contents")
+        
+        if self.hidesWhenStopped {
+            self.hidden = true
+        }
+    }
+    
+    func isAnimating () -> Bool {
+        return self.animating
     }
     
     // MARK: Init & Deinit
     
-    override init() {
-        super.init(frame: CGRectZero)
-        self.backgroundColor = UIColor.clearColor()
-        self.startAnimating()
-    }
-    
-    deinit {
-        self.stopAnimating()
+    override convenience init() {
+        self.init(frame: CGRectZero)
     }
     
     override init(frame: CGRect) {
-        super.init(frame: frame)
+        super.init(frame: CGRectZero)
         self.backgroundColor = UIColor.clearColor()
+        self.hidesWhenStopped = true
         self.startAnimating()
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.startAnimating()
@@ -92,54 +129,19 @@ func DegreesToRadians (value: Double) -> Double {
     
     // MARK: Private
     
-    private var animateIndex: Int = 1
-
-    private var animatedTimer: NSTimer?
-    
-    @objc private func animate () {
-        if !self.hidden {
-            self.setNeedsDisplay()
-            self.animateIndex++
-            if self.animateIndex > 8 {
-                self.animateIndex = 1
-            }
-        }
-    }
-    
-    override func drawRect(rect: CGRect) {
-        super.drawRect(rect)
-    
-        let context = UIGraphicsGetCurrentContext()
-        
-        var startAngle = Config.CC_ARC_DRAW_PADDING
-        for index in 1...8 {
-            let arcPath = UIBezierPath()
-            arcPath.addArcWithCenter(CGPointMake(CGFloat(self.frame.size.width/2), CGFloat(self.frame.size.height/2)), radius: CGFloat(Config.CC_ARC_DRAW_RADIUS), startAngle: CGFloat(DegreesToRadians(startAngle)), endAngle: CGFloat(DegreesToRadians(startAngle + Config.CC_ARC_DRAW_DEGREE)), clockwise: true)
-            CGContextAddPath(context, arcPath.CGPath)
-            startAngle += Config.CC_ARC_DRAW_DEGREE + (Config.CC_ARC_DRAW_PADDING * 2)
-            
-            CGContextSetLineWidth(context, CGFloat(Config.CC_ARC_DRAW_WIDTH))
-            let colorIndex = abs(index - self.animateIndex)
-            let strokeColor = Config.CC_ARC_DRAW_COLORS[colorIndex]
-            CGContextSetStrokeColorWithColor(context, strokeColor)
-            CGContextStrokePath(context)
-        }
-    }
+    private var animating = false
     
     override func intrinsicContentSize() -> CGSize {
-        return CGSizeMake(CGFloat(Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH), CGFloat(Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH))
+        return CGSizeMake(Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH, Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH)
     }
     
-    override func removeFromSuperview() {
-        self.stopAnimating()
-        super.removeFromSuperview()
+    override func sizeThatFits(size: CGSize) -> CGSize {
+        return CGSizeMake(Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH, Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH)
     }
     
-    override var hidden: Bool {
-        didSet {
-            if self.hidden {
-                self.stopAnimating()
-            }
+    override func layoutSubviews() {
+        if self.layer.bounds.size.width != Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH {
+            self.layer.bounds = CGRectMake(0, 0, Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH, Config.CC_ACTIVITY_INDICATOR_VIEW_WIDTH)
         }
     }
 }
